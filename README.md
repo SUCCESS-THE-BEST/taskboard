70278fae42fc6069489dc154bc92581534d66e89
Suxcedo <lesdfresh@gmail.com>
2026-02-18 19:42:30 +0200

    Ii created an initial commit for Taskboard
    - i added README.md with Node and npm versions
    - i then added hello-node.js logging a JSON object
    - i also added index.html, add-task.html, and about.html as intructed in the pdf
    - lastly i have added screenshots for evidence all in the same folder.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Taskboard Toolchain Setup
+
+## Node Version
+v22.14.0
+
+## npm Version
+10.9.2
---
+++ hello-node.js
@@ -0,0 +1,6 @@
+const data = {
+  name: "Taskboard",
+  message: "My node is working"
+};
+
+console.log(data);
